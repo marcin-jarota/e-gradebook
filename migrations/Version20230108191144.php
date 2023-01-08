0463a86b408bded27909a1a6fe8f12fdc51db2c2
Marcin <mjarota@greywizard.com>
2023-01-08 20:35:38 +0100

    cleanup migrations

--- migrations/Version20230108191144.php
+++ migrations/Version20230108191144.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230102193438 extends AbstractMigration
+final class Version20230108191144 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,20 +23,24 @@ final class Version20230102193438 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE class_group_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE instructor_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE mark_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE notification_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE student_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE subject_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE class_group (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE instructor (id INT NOT NULL, user_data_id INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_31FC43DD6FF8BF36 ON instructor (user_data_id)');
-        $this->addSql('CREATE TABLE mark (id INT NOT NULL, student_id_id INT NOT NULL, subject_id INT DEFAULT NULL, value NUMERIC(10, 0) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE mark (id INT NOT NULL, student_id_id INT NOT NULL, subject_id INT DEFAULT NULL, instructor_id INT DEFAULT NULL, value NUMERIC(10, 2) NOT NULL, created_at DATE NOT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_6674F271F773E7CA ON mark (student_id_id)');
         $this->addSql('CREATE INDEX IDX_6674F27123EDC87 ON mark (subject_id)');
+        $this->addSql('CREATE INDEX IDX_6674F2718C4FC193 ON mark (instructor_id)');
+        $this->addSql('CREATE TABLE notification (id INT NOT NULL, stakeholder_id INT NOT NULL, message VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_BF5476CAF2D3711A ON notification (stakeholder_id)');
         $this->addSql('CREATE TABLE student (id INT NOT NULL, class_group_id INT NOT NULL, user_data_id INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_B723AF334A9A1217 ON student (class_group_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_B723AF336FF8BF36 ON student (user_data_id)');
         $this->addSql('CREATE TABLE subject (id INT NOT NULL, name VARCHAR(50) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE users (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(50) NOT NULL, surname VARCHAR(50) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE users (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) DEFAULT NULL, name VARCHAR(50) NOT NULL, surname VARCHAR(50) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
@@ -53,6 +57,8 @@ final class Version20230102193438 extends AbstractMigration
         $this->addSql('ALTER TABLE instructor ADD CONSTRAINT FK_31FC43DD6FF8BF36 FOREIGN KEY (user_data_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE mark ADD CONSTRAINT FK_6674F271F773E7CA FOREIGN KEY (student_id_id) REFERENCES student (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE mark ADD CONSTRAINT FK_6674F27123EDC87 FOREIGN KEY (subject_id) REFERENCES subject (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE mark ADD CONSTRAINT FK_6674F2718C4FC193 FOREIGN KEY (instructor_id) REFERENCES instructor (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE notification ADD CONSTRAINT FK_BF5476CAF2D3711A FOREIGN KEY (stakeholder_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF334A9A1217 FOREIGN KEY (class_group_id) REFERENCES class_group (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF336FF8BF36 FOREIGN KEY (user_data_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
@@ -64,17 +70,21 @@ final class Version20230102193438 extends AbstractMigration
         $this->addSql('DROP SEQUENCE class_group_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE instructor_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE mark_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE notification_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE student_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE subject_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
         $this->addSql('ALTER TABLE instructor DROP CONSTRAINT FK_31FC43DD6FF8BF36');
         $this->addSql('ALTER TABLE mark DROP CONSTRAINT FK_6674F271F773E7CA');
         $this->addSql('ALTER TABLE mark DROP CONSTRAINT FK_6674F27123EDC87');
+        $this->addSql('ALTER TABLE mark DROP CONSTRAINT FK_6674F2718C4FC193');
+        $this->addSql('ALTER TABLE notification DROP CONSTRAINT FK_BF5476CAF2D3711A');
         $this->addSql('ALTER TABLE student DROP CONSTRAINT FK_B723AF334A9A1217');
         $this->addSql('ALTER TABLE student DROP CONSTRAINT FK_B723AF336FF8BF36');
         $this->addSql('DROP TABLE class_group');
         $this->addSql('DROP TABLE instructor');
         $this->addSql('DROP TABLE mark');
+        $this->addSql('DROP TABLE notification');
         $this->addSql('DROP TABLE student');
         $this->addSql('DROP TABLE subject');
         $this->addSql('DROP TABLE users');
